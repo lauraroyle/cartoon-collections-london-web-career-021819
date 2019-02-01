809b57562c1d5c6758a5b7174ea816c82bffb74a
Laura Royle <lauraamie.royle@gmail.com>
2019-02-01 12:11:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(foods)# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   foods.find do |food|
